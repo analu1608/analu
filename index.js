20d71af04180b3376346b56f74ceb957535e7660
analu1608 <analuizsantosdonascimento66@gmail.com>
2025-10-09 08:36:41 -0300

    adicionando mais conteúdo

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function mudar_conteudo_elemento() {
 
      document.getElementById("demo").innerHTML= "o clique está fucionando";
 }
+function mudar_cor_elemento(){
+ 
+     const paragrafo = document.querySelector('#paragrafo');
+     paragrafo.addEventListener('click'  ,  ()=> {
+
+     const elemento_paragrafo= document.querySelector('#paragrafo');
+     
+     elemento_paragrafo.style.color = 'red';
+
+     });
+ }
+
